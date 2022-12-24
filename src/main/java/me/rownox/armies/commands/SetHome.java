db6d5f82374bca17c58e6444aea3075ff46fc5a6
yes <fryrbeadcider@gmail.com>
2022-12-23 22:37:42 -0500

    kitcmd permission check

---
+++ src/main/java/me/rownox/armies/commands/SetHome.java
@@ -0,0 +1,18 @@
+package me.rownox.armies.commands;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+public class SetHome implements CommandExecutor{
+    @Override
+    public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
+        if(commandSender instanceof Player p){
+
+
+        }
+
+        return false;
+    }
+}
--- src/main/java/me/rownox/armies/events/JoinEvent.java
+++ src/main/java/me/rownox/armies/events/JoinEvent.java
@@ -1,5 +1,7 @@
 package me.rownox.armies.events;
 
+import org.bukkit.Bukkit;
+import org.bukkit.Location;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
@@ -11,7 +13,9 @@ public class JoinEvent implements Listener {
     public void onJoin(PlayerJoinEvent e){
         Player player = e.getPlayer();
         if (!player.hasPlayedBefore()) {
+            player.teleport(new Location(Bukkit.getWorld("world"),0,6,0,0,0));
             KitPlayer(player);
+
         }
     }
 }
