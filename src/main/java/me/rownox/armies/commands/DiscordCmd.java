a60234a16c7b1708d91d6c6675c8ff10d6d5bf41
Ntdi <79174456+n-tdi@users.noreply.github.com>
2023-02-23 20:06:28 -0500

    change commandSender

--- src/main/java/me/rownox/armies/commands/DiscordCmd.java
+++ src/main/java/me/rownox/armies/commands/DiscordCmd.java
@@ -5,18 +5,13 @@ import org.bukkit.ChatColor;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
-import org.bukkit.entity.Player;
 
 public class DiscordCmd implements CommandExecutor {
     @Override
     public boolean onCommand(CommandSender commandSender, Command command, String s, String[] strings) {
-        if (commandSender instanceof Player p) {
-
-            p.sendMessage("");
-            p.spigot().sendMessage(LinkUtils.discordInvite(ChatColor.AQUA + "[Click Here] " + ChatColor.DARK_AQUA + "to join the discord"));
-            p.sendMessage("");
-            return true;
-        }
-        return false;
+        commandSender.sendMessage("");
+        commandSender.spigot().sendMessage(LinkUtils.discordInvite(ChatColor.AQUA + "[Click Here] " + ChatColor.DARK_AQUA + "to join the discord"));
+        commandSender.sendMessage("");
+        return true;
     }
 }
